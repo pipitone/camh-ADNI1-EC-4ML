f07bccdccbd3c226d8c41c66ad08e1dd3d9b2171
Jon Pipitone <jon.pipitone@gmail.com>
2015-06-26 10:06:20 -0400

    Iniitial import, all labels transformed to mni space

---
+++ transform_candidates.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Transforms candidate labels in a tarbal
+ 
+tar=$1
+dest=$2
+tmp=$(mktemp -d)
+
+filestem=${tar/*ADNI/ADNI}
+filestem=${filestem/_everything.tar/}
+
+tar -C $tmp -xf $tar 
+
+xfm=reg_images/${filestem}.xfm
+model=/opt/quarantine/CIVET/1.1.12/build/share/mni-models/mni_adni_t1w_tal_nlin_asym.mnc
+regtmp=$(mktemp -d)
+
+i=0
+find $tmp -name labels.mnc | while read lbl; do 
+  mincresample -transform $xfm \
+    -quiet \
+    -keep -near \
+    -like $model \
+    $lbl \
+    $regtmp/${filestem}_${i}.mnc
+  i=$(($i + 1))
+done
+    
+tar cf $dest $regtmp
+rm -rf $tmp $regtmp
---
+++ transform_fused.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Transforms candidate labels in a tarbal
+
+native=$PWD/fused_labels
+dest=$PWD/transformed_fused
+
+model=/opt/quarantine/CIVET/1.1.12/build/share/mni-models/mni_adni_t1w_tal_nlin_asym.mnc
+
+for lbl in $native/*.mnc; do 
+  filestem=$(basename $lbl .mnc) 
+  xfm=reg_images/${filestem}.xfm
+  echo mincresample -transform $xfm \
+    -quiet \
+    -keep -near \
+    -like $model \
+    $lbl \
+    $dest/${filestem}.mnc
+done
